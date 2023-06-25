cf611e802c3ed19952c853416eb4810dbb59ef09
PrinceJha0066 <137476066+PrinceJha0066@users.noreply.github.com>
2023-06-25 20:17:18 +0530

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,10 @@
+# n1,n2,n3 = int(input("enter 3 numbers:"))
+# Total = n1+n2+n3
+# avg=Total/3
+# print(avg)
+
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=(a+b+c)/3
+print (d)
